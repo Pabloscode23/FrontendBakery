822a678204311e15846d4403975cd1d5abbbd00b
Jose Pablo Navarro <jnavarror@ucenfotec.ac.cr>
2025-02-06 20:28:34 -0600

    feat:add templates for dashboard and products cards

--- src/components/component-product-card/ComponentProductCard.tsx
+++ src/components/component-product-card/ComponentProductCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 interface ProductCardProps {
     image: string;
     title: string;
@@ -10,13 +11,13 @@ interface ProductCardProps {
 
 export const ComponentProductCard: React.FC<ProductCardProps> = ({ image, title, description, price }) => {
     return (
-        <div className='product-card__container flex flex-row h-[150px] w-[350px] rounded-md overflow-hidden bg-[var(--color-brown-light)] text-[30px]'>
+        <div className='product-card__container flex flex-row h-[150px] w-[370px] rounded-md overflow-hidden bg-[var(--color-brown-light)] text-[30px]'>
 
             <div className='product-card__image-container w-[40%] h-full'>
                 <img className='product-card__image w-full h-full object-cover' src={image} alt={title} />
             </div>
 
-            <div className='product-card__info-container w-[60%] flex flex-col justify-between pt-3 pb-4 pr-2 pl-[15px]'>
+            <div className='product-card__info-container w-[60%] flex flex-col justify-between pt-3 pb-4 pr-3 pl-[15px]'>
 
                 <div className='product-card__info'>
                     <h1 className='product-card__title text-base font-bold text-[var(--color-brown-darkest)] '>{title}</h1>
@@ -27,7 +28,7 @@ export const ComponentProductCard: React.FC<ProductCardProps> = ({ image, title,
                 <div className='product-card__bottom-info flex justify-between items-center'>
                     <p className='product-card__price text-sm font-semibold text-[var(--color-brown-dark)]'>${price.toFixed(2)}</p>
                     <button className='product-card__add-btn w-9 h-9 flex items-center justify-center rounded-full bg-[var(--color-brown-middle)] text-white text-lg cursor-pointer hover:bg-[var(--color-brown-dark)] transition duration-300 ease-in-out'>
-                        +
+                        <FontAwesomeIcon icon={faPlus} />
                     </button>
                 </div>
             </div>
