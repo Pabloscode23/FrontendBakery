92eeaf84d6c7ece92fffdef9fa44f1aeedc7339d
Santiago <santi.zeledon@gmail.com>
2025-03-08 15:00:37 -0600

    feat/login: installation of redux toolkit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,66 +1,99 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 
-
 type FormData = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 };
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
-    
-    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-    const onSubmit = async (data: FormData) => {
-        try {
-            await login(data.email, data.password);
-            navigate("/pages/PageDashboard"); 
-        } catch (error) {
-            console.error("Error en login:", error);
-        }
-    };
+  const navigate = useNavigate();
 
-    return (
-        <div className="flex justify-center items-center bg-[url('src/assets/img/background.jpg')] bg-cover bg-center h-screen">
-            <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-sm">
-                <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6 ">Iniciar Sesión</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">Correo Electrónico</label>
-                        <input
-                            id="email"
-                            type="email"
-                            {...register("email", { required: "El email es obligatorio" })}
-                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
-                        />
-                        {errors.email && <p className="text-sm text-red-500 mt-1">{errors.email.message}</p>}
-                    </div>
+  const onSubmit = async (data: FormData) => {
+    try {
+      await login(data.email, data.password);
+      navigate("/pages/PageDashboard");
+    } catch (error) {
+      console.error("Error en login:", error);
+    }
+  };
 
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">Contraseña</label>
-                        <input
-                            id="password"
-                            type="password"
-                            {...register("password", { required: "La contraseña es obligatoria" })}
-                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
-                        />
-                        {errors.password && <p className="text-sm text-red-500 mt-1">{errors.password.message}</p>}
-                    </div>
+  return (
+    <div className="flex justify-center items-center bg-[url('src/assets/img/background.jpg')] bg-cover bg-center h-screen">
+      <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6 ">
+          Iniciar Sesiï¿½n
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Correo ElectrÃ³nico
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email", { required: "El email es obligatorio" })}
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
+            />
+            {errors.email && (
+              <p className="text-sm text-red-500 mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
 
-                    <button type="submit" className="w-full py-2 px-4 text-white cursor-pointer rounded-md bg-[var(--color-brown-middle)] hover:bg-[var(--color-brown-dark)]">
-                        Ingresar
-                    </button>
-                </form>
-                <div className="mt-4 text-center">
-                    <p className="text-sm text-gray-600">
-                        ¿No tienes una cuenta?{" "}
-                        <Link to="/pages/register" className="text-indigo-500 hover:text-indigo-700">Registrarse</Link>
-                    </p>
-                </div>
-            </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Contraseï¿½a
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...register("password", {
+                required: "La contraseï¿½a es obligatoria",
+              })}
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
+            />
+            {errors.password && (
+              <p className="text-sm text-red-500 mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2 px-4 text-white cursor-pointer rounded-md bg-[var(--color-brown-middle)] hover:bg-[var(--color-brown-dark)]"
+          >
+            Ingresar
+          </button>
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-sm text-gray-600">
+            ï¿½No tienes una cuenta?{" "}
+            <Link
+              to="/pages/register"
+              className="text-indigo-500 hover:text-indigo-700"
+            >
+              Registrarse
+            </Link>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
